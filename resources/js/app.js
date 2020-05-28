edf289736fb5f774ac01fc410be3b59eb08d648d
aydevelop <ay.develop1@gmail.com>
2020-05-28 18:06:49 +0300

    modal

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,16 +3,17 @@
 require('./bootstrap');
 window.Vue = require('vue');
 
+import VModal from './modules/vue-modal'
 import Element from 'element-ui'
 import locale from 'element-ui/lib/locale/lang/ru-RU'
 import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(VModal, { dynamic: true, injectModalsContainer: true });
 Vue.use(Element, { locale });
 
 const app = new Vue({
     el: '#app',
     methods: {
-        addService() {
-            alert("addService");
-        }
+      
     }
 });
