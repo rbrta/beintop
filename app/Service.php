59f30c17b1232eb26890312cc1bba4ef0c81f047
kwonterdevs <kwonterdevs@gmail.com>
2020-05-28 10:06:45 +0300

    models

--- app/Service.php
+++ app/Service.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Tariff extends Model
+class Service extends Model
 {
     //
 }
--- database/migrations/2020_05_28_063803_create_orders_table.php
+++ database/migrations/2020_05_28_063803_create_orders_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTariffsTable extends Migration
+class CreateOrdersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateTariffsTable extends Migration
      */
     public function up()
     {
-        Schema::create('tariffs', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('category_id');
-            $table->string('name');
-            $table->integer('period');
-            $table->integer('price');
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('service_id');
+            $table->date('expiration_date');
+            $table->string('services_status');
             $table->timestamps();
         });
     }
@@ -30,6 +30,6 @@ class CreateTariffsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tariffs');
+        Schema::dropIfExists('orders');
     }
 }
