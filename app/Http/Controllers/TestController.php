d32a0ced7c4d0d755bfc1287f46ba4af74e1784d
aydevelop <ay.develop1@gmail.com>
2020-06-03 13:17:00 +0300

    fix

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -37,24 +37,6 @@ class TestController extends Controller
     public function callback()
     {   
         $post = $_POST;
-        // $post = array (
-        //     'ik_co_id' => '5ed3d7051ae1bd39008b457b',
-        //     'ik_co_prs_id' => '406649888130',
-        //     'ik_inv_id' => '220551724',
-        //     'ik_inv_st' => 'success',
-        //     'ik_inv_crt' => '2020-06-03 11:42:16',
-        //     'ik_inv_prc' => '2020-06-03 11:42:16',
-        //     'ik_trn_id' => '',
-        //     'ik_pm_no' => 'ID_2',
-        //     'ik_pw_via' => 'test_interkassa_test_xts',
-        //     'ik_am' => '1.44',
-        //     'ik_co_rfn' => '1.44',
-        //     'ik_ps_price' => '1.5',
-        //     'ik_cur' => 'RUB',
-        //     'ik_desc' => 'Payment Description',
-        //     'ik_sign' => 'e6zooRxL4w1CKmBA9KQtdA==',
-        // );
-
         $key = 'UpMgttstrxSG5xzI';
         $ik_id = '5ed3d7051ae1bd39008b457b';
         $dataSet = $post;
@@ -64,20 +46,19 @@ class TestController extends Controller
         array_push($dataSet, $key); // Adding secret key at the end of the string
         $signString = implode(':', $dataSet); // Concatenation calues using symbol ":"
         $sign = base64_encode(md5($signString, true)); // Get MD5 hash as binare view using generate string and code it in BASE64
-        
-        Log::debug($post['ik_sign'] . '  <====>  ' . $sign);
-        return $post['ik_sign'] . '  <====>  ' . $sign;
-        //$order_price = '1.44';
-        // if($dataSet['ik_co_id'] != $ik_id || $dataSet['ik_inv_st'] != 'success' 
-        // || $dataSet['ik_am'] != $order_price || $sign != $_POST['ik_sign']){
-        //     return "";
-        // }
+
+        $order_price = '1.44';
+        if($dataSet['ik_co_id'] != $ik_id || $dataSet['ik_inv_st'] != 'success' 
+        || $dataSet['ik_am'] != $order_price || $sign != $_POST['ik_sign']){
+            Log::debug('error');
+            return;
+        }
   
-        return "+";
         $order_id = $dataSet['ik_pm_no'];
         str_replace('ID_', '', $order_id);
         Order::where('id', $order_id)->update(['paid_status' => 'active']);
-
+        
+        Log::debug('success');
         return "success";
     }
 
