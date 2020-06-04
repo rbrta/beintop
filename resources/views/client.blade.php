4a4f0d1307dcfa202cf7cc1aff000dda6cd51406
kwonterdevs <kwonterdevs@gmail.com>
2020-06-04 17:50:52 +0300

    fix

--- resources/views/client.blade.php
+++ resources/views/client.blade.php
@@ -8,34 +8,7 @@
 </head>
 <body>
      <!-- ------------------------------------------------------------------------------- -->    
-     <header >
-        <div class="wrapper">
-            <div class="logo">
-                <img src="/images/logo.svg" alt="">
-                <span>Be-in-top</span>
-            </div>
-            <ul class="menu">
-                <li><i class="fas fa-star"></i> <a href="#">Наши преимущества</a></li>
-                <li><i class="fas fa-th"></i> <a href="#">Выбрать тариф</a></li>
-                <li><i class="fas fa-home"></i> <a href="/">На главную</a></li>
-                <li><i class="fas fa-user"></i> <a href="/client">Личный кабинет</a></li>
-            </ul>
-
-            <div id="menuBtn" class="mobile-menu">
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
-        </div>
-        <div id="menuMob" class="wrapper-mobile hide">
-            <div class="mobile-menu">
-                <div><a href="#">На главную</a></div>
-                <div><a href="#">Личный кабинет</a></div>
-                <div><a href="#">Наши преимущества</a></div>
-                <div><a href="#">Выбрать тариф</a></div>
-            </div>
-        </div>
-    </header>
+     @include('parts.header')
    
     
     <!-- ------------------------------------------------------------------------------- -->    
