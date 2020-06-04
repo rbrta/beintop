cc3ae340cc3063c2ad65543c9f2fd2d8bcaddaae
chiz-developer <chiz.kharkiv@gmail.com>
2020-06-04 13:32:46 +0300

    added logout route

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'HomeController@index');
 
+Route::get('/logout', function(){
+    auth()->logout();
+
+    return redirect('/login');
+});
+
 // Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'admin']], function() {
 Route::group(['prefix' => 'admin'], function() {
     Route::get('/', 'AdminController@index');
