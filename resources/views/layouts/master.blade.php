310fa2c3f03c3f2204d230cbdbfdf9b631eda858
chiz-developer <chiz.kharkiv@gmail.com>
2020-06-27 13:16:38 +0300

    links for documents

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -30,6 +30,7 @@
         }
     </script>
 
+    <script src="/js/fontawesome.min.js"></script>
     <script src="{{ asset('/js/main.js') }}"></script>
     <script src="{{ asset('/js/app.js') }}"></script>
     @yield('scripts')
--- resources/views/parts/footer.blade.php
+++ resources/views/parts/footer.blade.php
@@ -1,10 +1,19 @@
 <footer>
     <a href="#">
         <img src="/images/contacts1.svg" alt="">
+        <span>be_in_top</span>
+    </a>
+    <a href="mailto:be_in_top@gmail.com">
+        <img src="/images/contacts2.svg" alt="">
         <span>be_in_top@gmail.com</span>
     </a>
+
     <a href="#">
-        <img src="/images/contacts2.svg" alt="">
-        <span>be_in_top</span>
+        <i class="fas fa-file-alt"></i>
+        <span>Политика конфиденциальности</span>
+    </a>
+    <a href="#">
+        <i class="fas fa-file-alt"></i>
+        <span>Публичная оферта</span>
     </a>
 </footer>
