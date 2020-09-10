6bc8ed1ffbc8583a8f0c62b4b1255600393bce4a
chiz-developer <chiz.kharkiv@gmail.com>
2020-09-10 12:16:17 +0300

    test routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+Route::get('/testweb', function(){
+    return ['hello, web'];
+});
 
 /*
 |--------------------------------------------------------------------------
