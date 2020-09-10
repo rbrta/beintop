820a186f694ccf17f3ca3f19615cdb88e385ef23
jutiss <jutisbox@gmail.com>
2020-09-10 13:57:55 +0300

    Payment pages fix

--- nuxt/pages/tinkoff/success.vue
+++ nuxt/pages/tinkoff/success.vue
@@ -31,30 +31,29 @@ export default {
 
 .payment-message .wrapper {
   max-width: 1251px;
-  height: 342px;
-  box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.25);
+  box-shadow: 0 4px 20px rgba(0, 0, 0, 0.25);
   background-color: white;
   margin: 0 auto;
   border-radius: 35px;
   text-align: center;
+  padding: 60px 0;
 }
 
 .payment-message .wrapper .title {
   font-size: 42px;
   color: #984493;
-  padding-top: 79px;
+  line-height: 1;
 }
 
 .payment-message .wrapper a {
-  width: 315px;
-  height: 47px;
   background: linear-gradient(89.67deg, #E164BE -0.56%, #FF74D8 -0.55%, #FF985E 97.67%);
   border-radius: 50px;
   font-size: 20px;
   display: inline-block;
-  margin-top: 63px;
+  margin-top: 50px;
   color: white;
   text-decoration: none;
-  line-height: 2.2
+  line-height: 2.2;
+  padding: 5px 30px;
 }
 </style>
