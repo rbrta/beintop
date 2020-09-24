d998e85af062eccc35225c4fe12914e0701d13f3
jutiss <jutisbox@gmail.com>
2020-09-24 09:33:53 +0300

    Countdown test page

---
+++ nuxt/pages/test.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="login">
+    <div class="wrapper">
+      <client-only>
+        <countdown :time="500000" :interval="1000">
+          <template slot-scope="props">
+
+            <div class="expires">
+              <div class="expires__label">Осталось</div>
+              <div class="expires__days_count">{{ props.days }}</div>
+              <div class="expires__days">{{ $plur(props.days, $plurString.days)}}</div>
+
+              <div class="expires__timer">
+                <div class="value">{{ props.hours }} <span>{{ $plur(props.hours, $plurString.hours) }} </span></div>
+                <div class="delimeter">:</div>
+                <div class="value">{{ props.minutes }} <span>{{ $plur(props.minutes, $plurString.minutes) }}</span></div>
+                <div class="delimeter">:</div>
+                <div class="value">{{ props.seconds }} <span>{{ $plur(props.seconds, $plurString.seconds) }}</span></div>
+              </div>
+            </div>
+          </template>
+        </countdown>
+      </client-only>
+      <hr>
+      <client-only>
+        <countdown :time="500000" :interval="1000">
+          <template slot-scope="props">
+            {{ props.days }} {{ $plur(props.days, $plurString.days)}},
+            {{ props.hours }} {{ $plur(props.hours, $plurString.hours) }},
+            {{ props.minutes }} {{ $plur(props.minutes, $plurString.minutes) }},
+            {{ props.seconds }} {{ $plur(props.seconds, $plurString.seconds) }}
+          </template>
+        </countdown>
+      </client-only>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TestPage",
+}
+</script>
